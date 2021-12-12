b62ba95b6418300dfd7fe450bb7d4d3bcc2e73f3
Tackemichi-kun-tokyo-revengers <96024695+Tackemichi-kun-tokyo-revengers@users.noreply.github.com>
2021-12-12 18:34:47 -0300

    Add files via upload

---
+++ routes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const PersonagensController = require("./controllers/personagens");
+ 
+router.get("/personagens", PersonagensController.getAll);
+
+router.get("/animes/:id/personagens", PersonagensController.getByFilme);
+
+router.get("/personagens/:id", PersonagensController.getOne);
+
+router.post("/personagens", PersonagensController.create);
+
+router.put("/personagens/:id", PersonagensController.update);
+
+router.delete("/personagens/:id", PersonagensController.remove);
+
+module.exports = router;
